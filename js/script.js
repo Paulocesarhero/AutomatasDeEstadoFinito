cf88600d365d033509e9f0e0eaf41dd9bbd9e850
Paulo Cesar Hernandez Rosado <paulocesarhero@gmail.com>
2021-06-13 16:46:58 -0500

    En el proximo voya intentar agregar los diagramas desde el HTML

--- js/script.js
+++ js/script.js
@@ -1,169 +1,71 @@
-var dotIndex = 0;
-var graphviz = d3.select("#graph").graphviz()
-    .transition(function () {
-        return d3.transition("main")
-            .ease(d3.easeLinear)
-            .delay(500)
-            .duration(1500);
-    })
-    .logEvents(true)
-    .on("initEnd", render);
-
-function render() {
-    var dotLines = dots[dotIndex];
-    var dot = dotLines.join('');
-    graphviz
-        .renderDot(dot)
-        .on("end", function () {
-            dotIndex = (dotIndex + 1) % dots.length;
-            render();
-        });
-}
-let dots = [
-    //grafo por defecto
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a"] ',
-        '    e1 -> e0[label = "a"] ',
-        '    e0 -> e0[label = "b"] ',
-        '    e1 -> e1[label = "b"] ',
-        '    e1 -> e1[label = "c"] ',
-        '    e0 -> e0[label = "c"] ',
-        '}'
-    ],
-    //grafo e0 a e1 numero impar de a estado1
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a", color=red] ',
-        '    e1 -> e0[label = "a"] ',
-        '    e0 -> e0[label = "b"] ',
-        '    e1 -> e1[label = "b"] ',
-        '    e1 -> e1[label = "c"] ',
-        '    e0 -> e0[label = "c"] ',
-        '}'
-    ],
-    //grafo de e1 a e0 numero par de a estado0
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a"] ',
-        '    e1 -> e0[label = "a" , color=red] ',
-        '    e0 -> e0[label = "b"] ',
-        '    e1 -> e1[label = "b"] ',
-        '    e1 -> e1[label = "c"] ',
-        '    e0 -> e0[label = "c"] ',
-
-        '}'
-    ],
-    //grafo con numero par de a y alguna b estado 0
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a"] ',
-        '    e1 -> e0[label = "a"] ',
-        '    e0 -> e0[label = "b" , color=red] ',
-        '    e1 -> e1[label = "b"] ',
-        '    e1 -> e1[label = "c"] ',
-        '    e0 -> e0[label = "c"] ',
-        '}'
-    ],
-    //grafo con numero impar de a y alguna b estado1
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a"] ',
-        '    e1 -> e0[label = "a" ] ',
-        '    e0 -> e0[label = "b"] ',
-        '    e1 -> e1[label = "b", color=red,] ',
-        '    e1 -> e1[label = "c"] ',
-        '    e0 -> e0[label = "c"] ',
-        '}'
-    ],
-    //grafo con una c en el estado 0
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a"] ',
-        '    e1 -> e0[label = "a" ] ',
-        '    e0 -> e0[label = "b"] ',
-        '    e1 -> e1[label = "b"] ',
-        '    e1 -> e1[label = "c"] ',
-        '    e0 -> e0[label = "c", color=red,] ',
-        '}'
-    ],
-    //grafo con una c en el estado1
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a"] ',
-        '    e1 -> e0[label = "a" ] ',
-        '    e0 -> e0[label = "b"] ',
-        '    e1 -> e1[label = "b"] ',
-        '    e1 -> e1[label = "c", color=red,] ',
-        '    e0 -> e0[label = "c"] ',
-        '}'
-    ],
-
-    ''
-];
-dots.push(/*tendria que ir del e0 a e1*/
-    [
-        'digraph  {',
-        '    node [style="filled"]',
-        '    e0 [fillcolor="#d62728"]',
-        '    e1 [fillcolor="#1f77b4", shape="doublecircle"]',
-        '    e0 -> e1[label = "a", color=red,] ',
-        '    e1 -> e0[label = "a" ] ',
-        '    e0 -> e0[label = "b"] ',
-        '    e1 -> e1[label = "b"] ',
-        '    e1 -> e1[label = "c"] ',
-        '    e0 -> e0[label = "c"] ',
-        '}'
-    ]
-)
-
-console.log("Este es mi console log:  " + dots[8] + "Este es el tamño del array: " + dots.length)
-    //grafo e0 a e1 numero impar de as estado1
 
 // Empieza el codigo para sustraer la cadena
 
 
-let agregarLetra = document.querySelector("#agregarLetra").onclick = function (){agregarLaCadena()};
+let agregarLetra = document.querySelector("#agregarLetra").onclick = function () {
+    agregarLaCadena()
+};
 
 let seleccionarletra = document.querySelector("#inputGroupSelect01");
 
 let cadena = document.querySelector("#cadena");
 
+let botonIncializador = document.querySelector("#btn-check-outlined").onclick = function () {
+    incializarAnimacion()
+};
 
-function agregarLaCadena(){
-    switch (seleccionarletra.selectedIndex){
-        case 0:
-            cadena.value += 'a';
-            break
-        case 1:
-            cadena.value += 'b';
-            break
-        case 2:
-            cadena.value += 'c';
-            break
-        default:
-            cadena.value = cadena.value;
+function incializarAnimacion() {
+    let estado=false; /*Si es estado es falso quiere decir que no esta ene el estado de aceptacion*/
+    for (let i = 0; i < cadena.value.length; i++) {
+        if (cadena.value[i] === 'b') {
+            if (estado == true){
+                d3.select("#graph2").graphviz().renderDot('digraph  {e0 [fillcolor="#d62728"] e1 [fillcolor="#1f77b4", shape="doublecircle"] e0 -> e1[label = "a"] e1 -> e0[label = "a" ] e0 -> e0[label = "b"] e1 -> e1[label = "b", color=red]  e1 -> e1[label = "c"] e0 -> e0[label = "c"]  }');
+            }else{
+                d3.select("#graph2").graphviz().renderDot('digraph  {e0 [fillcolor="#d62728"] e1 [fillcolor="#1f77b4", shape="doublecircle"] e0 -> e1[label = "a"] e1 -> e0[label = "a" ] e0 -> e0[label = "b", color=red] e1 -> e1[label = "b"]  e1 -> e1[label = "c"] e0 -> e0[label = "c"]  }');
+            }
+            document.querySelector("#lebel2").innerHTML = i+1;
+
+        } else if (cadena.value[i] === 'a') {
+            if (estado === false){
+                d3.select("#graph1").graphviz().renderDot('digraph  {e0 [fillcolor="#d62728"] e1 [fillcolor="#1f77b4", shape="doublecircle"] e0 -> e1[label = "a", color=red] e1 -> e0[label = "a" ] e0 -> e0[label = "b"] e1 -> e1[label = "b"]  e1 -> e1[label = "c"] e0 -> e0[label = "c"]  }');
+                estado = true;
+            }else if (estado = true){
+                d3.select("#graph1").graphviz().renderDot('digraph  {e0 [fillcolor="#d62728"] e1 [fillcolor="#1f77b4", shape="doublecircle"] e0 -> e1[label = "a"] e1 -> e0[label = "a", color=red] e0 -> e0[label = "b"] e1 -> e1[label = "b"]  e1 -> e1[label = "c"] e0 -> e0[label = "c"]  }');
+                estado = true;
+            }
+
+            document.querySelector("#lebel1").innerHTML = i+1;
+
+        } else if (cadena.value[i] === 'c') {
+            if (estado == true){
+                d3.select("#graph3").graphviz().renderDot('digraph  {e0 [fillcolor="#d62728"] e1 [fillcolor="#1f77b4", shape="doublecircle"] e0 -> e1[label = "a"] e1 -> e0[label = "a" ] e0 -> e0[label = "b"] e1 -> e1[label = "b"]  e1 -> e1[label = "c", color=red] e0 -> e0[label = "c"]  }');
+            }else{
+                d3.select("#graph3").graphviz().renderDot('digraph  {e0 [fillcolor="#d62728"] e1 [fillcolor="#1f77b4", shape="doublecircle"] e0 -> e1[label = "a"] e1 -> e0[label = "a" ] e0 -> e0[label = "b"] e1 -> e1[label = "b"]  e1 -> e1[label = "c"] e0 -> e0[label = "c", color=red]  }');
+            }
+            document.querySelector("#lebel3").innerHTML = i+1;
+
+        }
+
+    }
+}
+
+function agregarLaCadena() {
+    if (cadena.value.length < 4) {
+        switch (seleccionarletra.selectedIndex) {
+            case 0:
+                cadena.value += 'a';
+                break
+            case 1:
+                cadena.value += 'b';
+                break
+            case 2:
+                cadena.value += 'c';
+                break
+            default:
+                cadena.value = cadena.value;
+        }
+    } else {
+        alert("No puedes ingresar mas valores")
     }
 
     // let cadena = document.querySelector("#cadena").value = "Hola mudo de nuevo";
